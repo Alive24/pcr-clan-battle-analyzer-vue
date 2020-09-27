e54c0f9f8a4b9dbdaf80b961d9c77acd5132a527
Alive24 <xct24@live.com>
2020-09-27 05:49:47 +0000

    Deploying to gh-pages from  @ d802e3c15aeb6431b04dff045c824eb3363d643b 🚀

---
+++ precache-manifest.bdaac3ede4ebcdd468e60108bbf832e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c23039cf3c42bd867c",
+    "url": "/css/chunk-vendors.33a97c46.css"
+  },
+  {
+    "revision": "b12c25edddf5d6ea11d90a6c22b624c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2829c90f9ad040773855",
+    "url": "/js/app.c0788d0f.js"
+  },
+  {
+    "revision": "b8c23039cf3c42bd867c",
+    "url": "/js/chunk-vendors.f6277f64.js"
+  },
+  {
+    "revision": "e6fd9a2bda172f6136cc15af314f3092",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
